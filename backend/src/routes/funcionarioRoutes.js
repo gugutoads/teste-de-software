a21578f90e9162ac4cc41a4e1931074f67443e4e
gugutoads <gugutoad@gmail.com>
2025-06-23 14:22:24 -0300

    Delete backend directory

--- backend/src/routes/funcionarioRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const funcionarioController = require('../controllers/funcionarioController');
-
-router.post('/login-funcionario', funcionarioController.loginFuncionario);
-
-module.exports = router;
--- backend/src/routes/verPedidosRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const verPedidosController = require('../controllers/verPedidosController');
-
-router.get('/', verPedidosController.getTodosPedidos);
-
-module.exports = router;
